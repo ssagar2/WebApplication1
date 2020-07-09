89066bf779ab08a8c1a9c577a9b8f2d7a5a995b4
swapnil sagar <ssagar2@uic.edu>
2020-07-09 14:29:12 -0500

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Second Page View - Space Query
+        public ActionResult SpaceQuery()
+        {
+            return View();
+        }
+
+        //Third Page - part1
+        public ActionResult Sticher()
+        {
+            return View();
+        }
+    }
+}
